0f7ac20306d17f28da2b680b6ba1cc328fd4d015
Adrien SAUNIER <asaunier@octo.com>
2016-12-16 14:40:50 +0100

    Day5: Ex1 - Clean

--- day5/door_decoder.rb
+++ day5/door_decoder.rb
@@ -6,8 +6,6 @@ class DoorDecoder
     hash = []
     index = 0
 
-    #Digest::MD5.hexdigest 'abc3231929'
-
     loop do
       break if hash.size == length
       md5 = Digest::MD5.hexdigest [door_id, index].join()
@@ -17,25 +15,6 @@ class DoorDecoder
 
       index += 1
     end
-
-    # length.times do
-    #   md5 = ""
-    #   loop do
-    #     md5 = Digest::MD5.hexdigest [door_id, index].join()
-    #
-    #     if md5.start_with?("00000")
-    #       p [door_id, index].join()
-    #     end
-    #
-    #     break if md5.start_with?("00000")
-    #
-    #     index += 1
-    #
-    #   end
-    #   hash.push(md5[5])
-    # end
-
-
 
     hash.join("")
   end
